7100c9ac7deeed2edb6841260b8854f7c5c1e208
harish479 <harishmudari@gmail.com>
2020-09-14 13:24:57 +0530

    Initial commit

---
+++ src/main/java/com/harishit/git/api/SpringBootWithGitHub2Application.java
@@ -0,0 +1,13 @@
+package com.harishit.git.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGitHub2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithGitHub2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/harishit/git/api/SpringBootWithGitHub2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harishit.git.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithGitHub2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
