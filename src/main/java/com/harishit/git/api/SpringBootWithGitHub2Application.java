75cc41e917da53de085c58089dcac73269500abf
harish479 <harishmudari@gmail.com>
2020-09-14 13:36:55 +0530

    main class modified

--- src/main/java/com/harishit/git/api/SpringBootWithGitHub2Application.java
+++ src/main/java/com/harishit/git/api/SpringBootWithGitHub2Application.java
@@ -2,12 +2,15 @@ package com.harishit.git.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootWithGitHub2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWithGitHub2Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SpringBootWithGitHub2Application.class, args);
+		run.close();
+
 	}
 
 }
